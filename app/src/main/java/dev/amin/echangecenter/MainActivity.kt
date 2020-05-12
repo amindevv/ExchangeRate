a50d24710246f644fdcd6d49530aded6f5dcdeaa
Amin Ahrabian <aahrabian@gmail.com>
2020-05-12 13:08:00 +0200

    Project Initial commit

---
+++ app/src/main/java/dev/amin/echangecenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.amin.echangecenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
