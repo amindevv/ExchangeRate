2fb9754238a5e7bfc0cd7eee3316181aab89d332
Amin Ahrabian <aahrabian@gmail.com>
2020-05-13 19:14:30 +0200

    Add MainActivity and project configs

--- app/src/main/java/dev/amin/echangecenter/MainActivity.kt
+++ app/src/main/java/dev/amin/echangecenter/MainActivity.kt
@@ -2,11 +2,22 @@ package dev.amin.echangecenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel =
+            ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.getExchangeRates()
+
+        btn.setOnClickListener {
+            viewModel.stopUpdates()
+        }
     }
 }
